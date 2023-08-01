a24c04bc10f23abb594b82898deabf3573670501
Nicolás <Nickogabii@gmail.com>
2023-08-01 01:51:03 -0300

    se agregaron funciones en la app producto

--- project/apps/producto/urls.py
+++ project/apps/producto/urls.py
@@ -10,4 +10,10 @@ urlpatterns = [
     path("", views.index, name="home"),
     path("02lista_productos/list/", views.list_product, name="02lista_producto"),
     path("01crear_producto/create/", views.create_product, name="01crear_producto"),
+    path("03detail_producto/<int:pk>/",
+         views.detail_product, name="03detail_producto"),
+    path("04actualizar_producto/<int:pk>/",
+         views.update_product, name="01crear_producto"),
+    path("05delete_producto/<int:pk>/",
+         views.delete_product, name="05delete_producto"),
 ]
--- project/apps/producto/views.py
+++ project/apps/producto/views.py
@@ -1,4 +1,5 @@
 from unicodedata import category
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 
 from . import models
@@ -13,7 +14,6 @@ def index(request):
 
 
 # Lista Producto
-
 def list_product(request):
     categorias = models.ProductoCategoriaa.objects.all()
     context = {"objects_list": categorias}
@@ -31,3 +31,34 @@ def create_product(request):
     else:
         form = forms.ProductoCategoriaaForm()
     return render(request, "producto/01crear_producto.html", {"form": form})
+
+# Detalles Producto
+
+
+def detail_product(request, pk):
+    query = models.ProductoCategoriaa.objects.get(id=pk)
+    return render(request, "producto/03detail_producto.html", {"object": query})
+
+# Actualizar Producto
+
+
+def update_product(request, pk):
+    query = models.ProductoCategoriaa.objects.get(id=pk)
+    if request.method == "POST":
+        form = forms.ProductoCategoriaaForm(request.POST, instance=query)
+        if form.is_valid():
+            form.save()
+            return redirect("producto:home")
+    else:
+        form = forms.ProductoCategoriaaForm(instance=query)
+    return render(request, "producto/01crear_producto.html", {"form": form})
+
+# Borrar Producto
+
+
+def delete_product(request: HttpRequest, pk: int) -> HttpResponse:
+    query = models.ProductoCategoriaa.objects.get(id=pk)
+    if request.method == "POST":
+        query.delete()
+        return redirect("producto:02lista_producto")
+    return render(request, "producto/05delete_producto.html", {"object": query})
