3383a1cd5dde55d6953a7147aa469f46afdaedd6
Nicolás <Nickogabii@gmail.com>
2023-08-07 19:46:21 -0300

    upload seccion imagenes

--- project/config/urls.py
+++ project/config/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 
 # ! include sirve para traer urls de las aplicaciones
@@ -34,3 +36,7 @@ urlpatterns = [
     path("blog03PC/", include("blog03PC.urls")),
     path("customerx/", include("customerx.urls")),
 ]
+
+if settings.DEBUG == True:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
